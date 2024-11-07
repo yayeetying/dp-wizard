b2e50b473a4e6d20f40196969c40effc07caa77e
Chuck McCallum <mccalluc@users.noreply.github.com>
2024-11-07 13:52:21 -0500

    Do not label every bin (#148)
    * perhaps gratuitous?
    
    * more idiomatic use of major and minor bins

--- dp_creator_ii/app/components/plots.py
+++ dp_creator_ii/app/components/plots.py
@@ -15,9 +15,14 @@ def _df_to_columns(df):
 
 
 def plot_histogram(histogram_df, error, cutoff):  # pragma: no cover
-    labels, values = _df_to_columns(histogram_df)
+    bins, values = _df_to_columns(histogram_df)
+    mod = (len(bins) // 12) + 1
+    majors = [label for i, label in enumerate(bins) if i % mod == 0]
+    minors = [label for i, label in enumerate(bins) if i % mod != 0]
     _figure, axes = plt.subplots()
     bar_colors = ["blue" if v > cutoff else "lightblue" for v in values]
-    axes.bar(labels, values, color=bar_colors, yerr=error)
+    axes.bar(bins, values, color=bar_colors, yerr=error)
+    axes.set_xticks(majors, majors)
+    axes.set_xticks(minors, ["" for _ in minors], minor=True)
     axes.axhline(cutoff, color="lightgrey", zorder=-1)
     # TODO: Since this seems to return None, how does the information flow?
