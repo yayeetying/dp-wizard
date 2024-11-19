7de7bb514d3bff82cf907cbf66dda17ab14d165d
Chuck McCallum <mccalluc@users.noreply.github.com>
2024-11-19 17:17:02 -0500

    Write to a relative temp dir (#167)

--- dp_wizard/utils/argparse_helpers.py
+++ dp_wizard/utils/argparse_helpers.py
@@ -81,10 +81,10 @@ def _get_demo_csv_contrib():
     """
     random.seed(0)  # So the mock data will be stable across runs.
 
-    csv_path = "/tmp/demo.csv"
+    csv_path = Path(__file__).parent.parent / "tmp" / "demo.csv"
     contributions = 10
 
-    with open(csv_path, "w", newline="") as demo_handle:
+    with csv_path.open("w", newline="") as demo_handle:
         fields = ["student_id", "class_year", "hw_number", "grade"]
         writer = csv.DictWriter(demo_handle, fieldnames=fields)
         writer.writeheader()
