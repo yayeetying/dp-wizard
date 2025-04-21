d068933a89531b748ec395bfb92c7baa0a9f086d
Chuck McCallum <mccalluc@users.noreply.github.com>
2025-04-21 14:39:49 -0400

    Capture environment info, and prepopulate github issue (#349)
    * prefilled issue
    
    * add some tests
    
    * style button
    
    * Not sure if this is simpler
    
    * Revert "Not sure if this is simpler"
    
    This reverts commit 3cd7e0a104b186ee7e115d317214d53cf549b176.
    
    * can not remember why I added action button
    
    * more url parsing in doctest
    
    * shorter lines

--- dp_wizard/app/about_panel.py
+++ dp_wizard/app/about_panel.py
@@ -1,13 +1,78 @@
 import sys
 from pathlib import Path
+import subprocess
+import urllib.parse
 
+from htmltools import tags
 from shiny import ui, reactive, Inputs, Outputs, Session
 
 from dp_wizard.app.components.outputs import nav_button
 
 
+def _run(cmd):
+    """
+    >>> _run("echo hello")
+    '    hello'
+    """
+    # Do not check exit status:
+    # If there is a problem, we don't want to worry about it.
+    return "\n".join(
+        f"    {line}"
+        for line in subprocess.run(cmd.split(" "), capture_output=True)
+        .stdout.decode()
+        .splitlines()
+    )
+
+
+def _get_info():
+    version = (Path(__file__).parent.parent / "VERSION").read_text().strip()
+    git_status = _run("git status")
+    pip_freeze = _run("pip freeze")
+    return f"""
+DP Wizard v{version}
+python: {sys.version}
+arguments: {' '.join(sys.argv[1:])}
+git status:
+{git_status}
+pip freeze:
+{pip_freeze}
+    """
+
+
+def _make_issue_url(info):
+    """
+    >>> info = 'A B C'
+    >>> query = urllib.parse.urlparse(_make_issue_url(info)).query
+    >>> print(urllib.parse.unquote_plus(query))
+    body=Please describe the problem.
+    <BLANKLINE>
+    <details>
+    The information below will help us reproduce the issue.
+    <BLANKLINE>
+    ```
+    A B C
+    ```
+    <BLANKLINE>
+    </details>
+    """
+    markdown = f"""Please describe the problem.
+
+<details>
+The information below will help us reproduce the issue.
+
+```
+{info}
+```
+
+</details>"""
+    encoded = urllib.parse.quote_plus(markdown)
+    return f"https://github.com/opendp/dp-wizard/issues/new?body={encoded}"
+
+
 def about_ui():
-    version = (Path(__file__).parent.parent / "VERSION").read_text()
+    info = _get_info()
+    issue_url = _make_issue_url(info)
+
     return ui.nav_panel(
         "About",
         ui.card(
@@ -24,8 +89,19 @@ def about_ui():
                 - Text and CSV reports.
                 """
             ),
-            ui.p(f"DP Wizard version {version}"),
-            ui.p(f"Python {sys.version}"),
+            tags.textarea(
+                info,
+                readonly=True,
+                rows=10,
+                style="font-family: monospace;",
+            ),
+            ui.a(
+                "File issue",
+                href=issue_url,
+                target="_blank",
+                class_="btn btn-default",
+                style="width: 10em;",
+            ),
         ),
         nav_button("go_to_dataset", "Select dataset"),
         value="about_panel",
