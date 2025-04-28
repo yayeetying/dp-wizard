c34c31097766fb4f9be613edec0a7f493def162e
Yaying Liang Li <72949647+yayeetying@users.noreply.github.com>
2025-04-27 21:29:17 -0400

    finished count query

--- dp_wizard/utils/code_generators/analyses/count/no-tests/_count_expr.py
+++ dp_wizard/utils/code_generators/analyses/count/no-tests/_count_expr.py
@@ -1,4 +1,4 @@
-# See the OpenDP docs for more on making private means:
+# See the OpenDP docs for more on making private counts:
 # https://docs.opendp.org/en/stable/getting-started/tabular-data/essential-statistics.html#Count
 
 EXPR_NAME = (
@@ -6,5 +6,6 @@ EXPR_NAME = (
     .cast(float)
     .fill_nan(0)
     .fill_null(0)
-    .dp.mean((LOWER_BOUND, UPPER_BOUND))
+    .dp.clamp(LOWER_BOUND, UPPER_BOUND)
+    .dp.len()
 )
