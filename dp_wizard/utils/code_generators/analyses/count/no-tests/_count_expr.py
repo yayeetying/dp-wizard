1590930cdfc4f7d14e7697c0249a2a13f8f2e8b6
Yaying Liang Li <72949647+yayeetying@users.noreply.github.com>
2025-04-30 15:31:50 -0400

    downloading count works now

--- dp_wizard/utils/code_generators/analyses/count/no-tests/_count_expr.py
+++ dp_wizard/utils/code_generators/analyses/count/no-tests/_count_expr.py
@@ -2,10 +2,5 @@
 # https://docs.opendp.org/en/stable/getting-started/tabular-data/essential-statistics.html#Count
 
 EXPR_NAME = (
-    pl.col(COLUMN_NAME)
-    .cast(float)
-    .fill_nan(0)
-    .fill_null(0)
-    .dp.clamp(LOWER_BOUND, UPPER_BOUND)
-    .dp.len()
+    pl.col(COLUMN_NAME).cast(float).fill_nan(0).fill_null(0).dp.count().alias("count")
 )
