8acc10526bf9328362fb039ec5f9e3bda737ccc3
Yaying Liang Li <72949647+yayeetying@users.noreply.github.com>
2025-05-05 16:37:27 -0400

    some new changes

--- dp_wizard/utils/code_generators/analyses/stdeviation/no-tests/_stdeviation_expr.py
+++ dp_wizard/utils/code_generators/analyses/stdeviation/no-tests/_stdeviation_expr.py
@@ -1,6 +1,5 @@
 # See the OpenDP docs for more on making private Standard Devation releases:
 
-"""
 from opendp.mod import enable_features
 from opendp.transformations import make_variance
 from opendp.measurements import make_laplace
@@ -9,20 +8,44 @@ from opendp.prelude import atom_domain, vector_domain
 import math
 
 enable_features("contrib")
-"""
 
+# Variance Transformation
+# Calculate bounded domain
+float_dom = atom_domain(T=float, nan=True, bounds=(LOWER_BOUND, UPPER_BOUND))
+# Size is dependent on size of dataset
+vec_dom = vector_domain(float_dom, size=100)
+metric = SymmetricDistance()
+bounded_variance = make_variance(input_domain=vec_dom, input_metric=metric)
+
+print(bounded_variance)
+
+# Build a Laplace mechanism that accepts bounded_variance's output
+# Add Laplace noise; scale should be b = sensitivity (10; number of contributions) / epsilon
+lap_mech = make_laplace(
+    bounded_variance.output_domain, bounded_variance.output_metric, scale=10 / 1
+)
+
+
+def dp_variance(dataset):
+    return bounded_variance >> lap_mech
+
+
+# Standard Deviation
+EXPR_NAME = math.sqrt(
+    dp_variance(pl.col(COLUMN_NAME).cast(float).fill_nan(0).fill_null(0))
+)
+
+"""
 import polars as pl
-import pandas as pd
 import numpy as np
 
 
 def private_std(data):
-    data = np.clip(data, LOWER_BOUND, UPPER_BOUND)
+    data = np.clip(data, 0.0, 10.0)
     mean = sum(data) / len(data)
     squared_devs = [(x - mean) ** 2 for x in data]
     stdeviation = sum(squared_devs) / len(data)
     return stdeviation
-
 
 # arr = np.array(pl.col(COLUMN_NAME).cast(float).fill_nan(0).fill_null(0).to_numpy())
 
@@ -31,10 +54,9 @@ EXPR_NAME = (
     .cast(float)
     .fill_nan(0)
     .fill_null(0)
-    .dp.mean((LOWER_BOUND, UPPER_BOUND))
     .alias("standard_deviation")
 )
-
+"""
 """
 import polars as pl
 
@@ -44,11 +66,12 @@ EXPR_NAME = (
       .fill_nan(0.0)
       .fill_null(0.0)
       .clip(LOWER_BOUND, UPPER_BOUND)
-      .dp.mean((LOWER_BOUND, UPPER_BOUND))
-      # .std(ddof=0)
+      # .dp.mean((LOWER_BOUND, UPPER_BOUND))
+      .std(ddof=0)
       .alias("private_standard_deviation")
 )
 """
+
 
 """
 def dp_var(data, lower, upper, size=100, epsilon=1):
@@ -67,34 +90,6 @@ EXPR_NAME = math.sqrt(
 )
 """
 
-"""
-# Variance Transformation
-# Calculate bounded domain
-float_dom = atom_domain(T=float, nan=False, bounds=(LOWER_BOUND, UPPER_BOUND))
-# Size is dependent on size of dataset
-vec_dom = vector_domain(float_dom, size=100)
-metric = SymmetricDistance()
-bounded_variance = make_variance(
-    input_domain=vec_dom,
-    input_metric=metric
-)
-
-# Build a Laplace mechanism that accepts bounded_variance's output
-# Add Laplace noise; scale should be b = sensitivity (10; number of contributions) / epsilon
-lap_mech = make_laplace(
-    bounded_variance.output_domain,
-    bounded_variance.output_metric,
-    scale= 1 / epsilon
-)
-
-def dp_variance(dataset):
-    return bounded_variance >> lap_mech
-
-# Standard Deviation
-EXPR_NAME = math.sqrt(
-    dp_variance(pl.col(COLUMN_NAME).cast(float).fill_nan(0).fill_null(0))
-)
-"""
 """
 import polars as pl
 import opendp.prelude as dp
